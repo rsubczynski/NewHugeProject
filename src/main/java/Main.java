f9e8efb7bb4d3e024bf005d5cf2e7931c7c1c00f
Radoslaw Subczynski <radoslaw.subczynski@abastra.com>
2018-01-21 17:03:57 +0100

    proba

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import javax.swing.*;
 
 public class Main
@@ -25,45 +26,66 @@ public class Main
 class ComboBoxFrame extends JFrame
 {
 
-    private static final int DEFAULT_WIDTH = 500;
-    private static final int DEFAULT_HEIGHT = 400;
+    private static final int DEFAULT_WIDTH = 800;
+    private static final int DEFAULT_HEIGHT = 200;
     private JComboBox faceCombo, faceCombo2 ;
     private JLabel label;
     private Button button;
+    private ArrayList<Stade> stadeArrayList = new ArrayList<>();
+    private int caschPositions;
 
     public ComboBoxFrame()
     {
+        stadeArrayList.add(new Stade("Alabama",1));
+        stadeArrayList.add(new Stade("Blabama",2));
+        stadeArrayList.add(new Stade("Clabama",3));
+
+
+
         setTitle("ComboBoxTest");
         setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
 
 
-        button = new Button("*");
-        final JLabel label1 = new JLabel("Tutaj bedzie wynik");
+        button = new Button("=");
+        final JLabel endingPrice = new JLabel("Cena Koncowa");
+        final JLabel percent = new JLabel("%");
+        final JLabel result = new JLabel("Wyliczenie");
 
 
-
-        faceCombo = new JComboBox();
-        faceCombo.setEditable(true);
-        for(Integer i : Generator.GenerateNumberList()) {
-            faceCombo.addItem(i);
-        }
 
 
         faceCombo2 = new JComboBox();
         faceCombo2.setEditable(true);
-        for(Integer i : Generator.GenerateRandomNumberList()) {
+        for(Integer i : Generator.GenerateNumberList()) {
             faceCombo2.addItem(i);
         }
 
+
+        faceCombo = new JComboBox();
+        faceCombo.setEditable(true);
+        for(Stade i : stadeArrayList) {
+            faceCombo.addItem(i.name);
+            caschPositions = i.getPrecent();
+        }
 
         button.addActionListener(new ActionListener()
         {
             public void actionPerformed(ActionEvent e)
             {
 
-                int wynik = Integer.valueOf(String.valueOf(faceCombo2.getSelectedItem())) * Integer.valueOf(String.valueOf(faceCombo2.getSelectedItem()));
+                float wynik =0;
 
-                label1.setText(String.valueOf(wynik));
+                int liczba =  (int) faceCombo2.getSelectedItem();
+                Stade stade = stadeArrayList.get(faceCombo.getSelectedIndex());
+
+                int procent = stade.getPrecent();
+
+                float wynikProcentowy =  procent/100f;
+                float cos = 1 + wynikProcentowy;
+
+                wynik = (liczba / cos);
+
+                result.setText(String.valueOf(wynik));
 
             }
         });
@@ -71,13 +93,17 @@ class ComboBoxFrame extends JFrame
         JPanel comboPanel = new JPanel();
         comboPanel.add(faceCombo);
         add(comboPanel, BorderLayout.BEFORE_FIRST_LINE);
+        comboPanel.add(percent);
 
         JPanel comboPanel2 = new JPanel();
         comboPanel.add(faceCombo2);
         add(comboPanel2, BorderLayout.CENTER);
 
         button.setSize( 50, 75 );
+        comboPanel.add(endingPrice);
         comboPanel.add(button);
-        comboPanel.add(label1);
+
+        comboPanel.add(result);
+
     }
 }
---
+++ src/main/java/Stade.java
@@ -0,0 +1,17 @@
+public class Stade {
+    public String name;
+    public int Precent;
+
+    public Stade(String name, int precent) {
+        this.name = name;
+        Precent = precent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrecent() {
+        return Precent;
+    }
+}
